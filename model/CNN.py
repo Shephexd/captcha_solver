740e0125e5a9f0c10503fe811a1a4bdef2956ac7
shephexd <shephexd@gmail.com>
2019-09-17 23:57:41 +0900

    WIP ADD CNN Base model need to define cost function

---
+++ model/CNN.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+
+X = tf.placeholder(tf.float32, shape=(None, 60, 160, 3))
+y = tf.placeholder(tf.float32, shape=(None, 5, 10))
+conv1 = tf.layers.conv2d(inputs=X,
+                        filters=32,
+                        kernel_size=[5, 5],
+                        padding='same',
+                        activation=tf.nn.relu)
+pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+conv2 = tf.layers.conv2d(inputs=input_layer,
+                        filters=64,
+                        kernel_size=[5, 5],
+                        padding='same',
+                        activation=tf.nn.relu)
+pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+pool2_flat = tf.reshape(pool2, [-1, 30 * 80 * 64])
+dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=True)
+logits = tf.layers.dense(inputs=dropout, units=50)
+
+reshaped_logits = tf.reshape(logits, [-1, 5, 10])
+
+
+if __name__ == '__main__':
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        output = sess.run(logits, feed_dict={X: input_data.reshape(-1, 60, 160, 3)/255})
+    print(output)

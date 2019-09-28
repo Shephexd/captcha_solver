c27ec12aafbfa03891546c2dfe1001a9eebecd0f
shephexd <shephexd@gmail.com>
2019-09-28 15:34:43 +0900

    UPDATE load_data() arguments

--- preprocess/load_data.py
+++ preprocess/load_data.py
@@ -3,14 +3,18 @@ import scipy.misc
 import numpy as np
 
 
-def get_data(pardir, debug=False):
+def get_data(pardir, max_size=None, debug=False):
     file_list = os.listdir(pardir)
+
+    if max_size is None:
+        max_size = len(file_list)
 
     x_data = list()
     y_data = list()
 
-    for f in file_list:
-        im = scipy.misc.imread('data/{}'.format(f), flatten=False, mode='RGB')
+    for f in file_list[:max_size]:
+        data_path = os.path.join(pardir, f)
+        im = scipy.misc.imread(data_path, flatten=False, mode='RGB')
         ims = scipy.misc.imresize(im,(60, 160, 3))
         x_data.append(ims)
         num = f.split('.')[0]
@@ -25,6 +29,7 @@ def get_data(pardir, debug=False):
     y_data = np.array(y_data)
 
     return x_data, y_data
+
 
 if __name__ == '__main__':
     (X, Y) = get_data()

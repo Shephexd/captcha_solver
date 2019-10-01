6d5522800fdc0b1a9122a9fb825e1b13c87ad52d
shephexd <shephexd@gmail.com>
2019-10-01 22:18:50 +0900

    ADD BatchNormalization Layer for generator

--- model/v1/gan/generator.py
+++ model/v1/gan/generator.py
@@ -14,7 +14,10 @@ class CaptchaGenerator(AbsNeuralNetwork):
                                          name='gen_conv1',
                                          activation=tf.nn.relu,
                                          trainable=True)
-            gen_conv2 = tf.layers.conv2d(inputs=gen_conv1,
+            conv1_flat = tf.reshape(gen_conv1, [-1, self.get_flatten_size(gen_conv1)])
+            normalized_conv1_flat = tf.layers.batch_normalization(conv1_flat, name='gen_batch_norm1')
+            normalized_conv1 = tf.reshape(normalized_conv1_flat, [-1, *self.get_tensor_shape(gen_conv1)[1:]])
+            gen_conv2 = tf.layers.conv2d(inputs=normalized_conv1,
                                          filters=3,
                                          kernel_size=[5, 5],
                                          padding='same',
@@ -22,11 +25,12 @@ class CaptchaGenerator(AbsNeuralNetwork):
                                          activation=tf.nn.relu,
                                          trainable=True)
 
-            conv2_flat = tf.reshape(gen_conv2, [-1, np.prod([*self.feature_shape])])
-            min_v = tf.reduce_min(conv2_flat)
-            max_v = tf.reduce_max(conv2_flat)
-            normalized_flat = (conv2_flat - min_v) / (max_v - min_v)
-            output = tf.reshape(normalized_flat, [-1, *self.feature_shape])
+            conv2_flat = tf.reshape(gen_conv2, [-1, self.get_flatten_size(gen_conv2)])
+            normalized_conv2_flat = tf.layers.batch_normalization(conv2_flat, name='gen_batch_norm2')
+            min_v = tf.reduce_min(normalized_conv2_flat)
+            max_v = tf.reduce_max(normalized_conv2_flat)
+            normalized_conv2_flat = (normalized_conv2_flat - min_v) / (max_v - min_v)
+            output = tf.reshape(normalized_conv2_flat, [-1, *self.feature_shape])
             
             self.tf_nodes['x_placeholder'] = gen_x_placeholder
             self.tf_nodes['gen_conv1'] = gen_conv1
@@ -34,6 +38,12 @@ class CaptchaGenerator(AbsNeuralNetwork):
             self.tf_nodes['output'] = output
             init = tf.global_variables_initializer()
             self.tf_nodes['init'] = init
+
+    def get_tensor_shape(self, tensor):
+        return tensor.shape.as_list()
+
+    def get_flatten_size(self, tensor):
+        return np.prod([i for i in self.get_tensor_shape(tensor) if i])
 
     def get_faked_dataset(self, n_sample):
         features = self.generate_captcha(n_sample)
@@ -47,6 +57,7 @@ class CaptchaGenerator(AbsNeuralNetwork):
         with self.graph.as_default():
             rand_inputs = np.random.normal(size=(n_sample, *self.feature_shape))
             rand_inputs = self.reshape_features(rand_inputs)
+            # 153600 values, but the requested shape requires a multiple of 28800
             return self.sess.run(self.tf_nodes['output'], feed_dict={self.tf_nodes['x_placeholder']: rand_inputs})
 
     def generate_faked_labels(self, n_sample):
@@ -69,3 +80,4 @@ if __name__ == '__main__':
     sess.run(generator.tf_nodes['init'])
     generated = generator.generate_captcha(4)
     print(generated)
+    print(generated.shape)
